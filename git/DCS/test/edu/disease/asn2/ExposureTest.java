4d138629f2660528e6adeedd0b6372667731188b
Mohamed Jameel Afsar <98633491+git-jameel@users.noreply.github.com>
2023-06-20 16:10:03 +0530

    ExposureTest.java

--- git/DCS/test/edu/disease/asn2/ExposureTest.java
+++ git/DCS/test/edu/disease/asn2/ExposureTest.java
@@ -1,14 +1,43 @@
-package edu.disease.asn2;
+package edu.disease.asn1;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExposureTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Exposure exposure;
+	@Before
+	public void setData()
+	{
+		UUID id = UUID.randomUUID();
+		exposure = new Exposure(id);
+		exposure.setDateTime(LocalDateTime.now());
+		exposure.setExposureType("I");
 	}
-
+	@Test
+	public void testExposureNegative()
+	{
+		assertNotNull(exposure.getDateTime());
+		assertNotNull(exposure.getExposureType());
+		assertNotNull(exposure.hashCode());
+		assertEquals("I", exposure.getExposureType());
+		assertNotNull(exposure.toString());
+		Exposure exposureobj = exposure;
+		assertTrue(exposure.equals(exposureobj));
+		assertTrue(exposure.equals(exposure));
+		assertFalse(exposure.equals(null));
+		Exposure exposureobjEquals = new Exposure(exposure.getPatientid());
+		exposureobjEquals.setExposureType("D");
+		exposureobjEquals.setDateTime(exposure.getDateTime());
+		assertTrue(exposure.equals(exposureobjEquals));
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testException()
+	{
+		exposure.setExposureType("Invalid");
+	}
 }
