28eea2c78e37221871a9d5539c00aaf2e66cdf74
Mohamed Jameel Afsar <98633491+git-jameel@users.noreply.github.com>
2023-06-26 15:55:10 +0530

    Exposure.java

--- git/DCS/src/edu/disease/asn1/Exposure.java
+++ git/DCS/src/edu/disease/asn1/Exposure.java
@@ -1,60 +1,105 @@
 package edu.disease.asn1;
+import java.util.Objects;
 import java.util.UUID;
 import java.time.LocalDateTime;
 
 public class Exposure {
-	private UUID patientid;
+	
+	//unique id to identify patients
+	private UUID patientId;
+	
+	//Local date time of a patient
 	private LocalDateTime dateTime;
+	
+	//Disease exposure type (direct exposure or indirect exposure)
 	private String exposureType;
 	
-	public Exposure(UUID patientid) { 
-		//Constructor to set id
-		this.patientid = patientid;
-	}
+	/**
+	 * Constructs a new {@link Exposure}
+	 * @param patientId
+	 * initialize {@link Exposure} class with unique id of patient
+	 */
 	
-	/*
-	 * // public static void main(String[] args) { // UUID uuid = new
-	 * UUID((int)2,(int)0); // Exposure expo = new Exposure(uuid); //
-	 * expo.setExposureType("I"); //
-	 * System.out.println("Patient id:\t\t DateTime:\t\t ExposureType"); //
-	 * System.out.println(expo.patientid+"\t"+LocalDateTime.now()+"\t "+expo.
-	 * exposureType); // //Patient p = new Patient(5,5); // }
+	public Exposure(UUID patientId) { 
+		this.patientId = patientId;
+	} 
+	
+	/**
+	 * Returns patient id *
+	 * @return patientId
 	 */
 
-	public UUID getPatientid() { //Patient id getter
-		return patientid;
+	public UUID getpatientId() { //Patient id getter
+		return patientId;
 	}
+	
 
+    /**
+     * @param patientId set the Id of patient
+     */
+    public void setPatientId(UUID patientId) {
+        this.patientId = patientId;
+    }
 
+	/**
+	 * Returns local date and time
+	 * @return dateTime
+	 */
 	public LocalDateTime getDateTime() { //DateTime getter
 		return dateTime;
 	}
 
+	/**
+	 * set Local Date Time of patient
+	 * @param dateTime
+	 */
 
 	public void setDateTime(LocalDateTime dateTime) { //DateTime setter
 		this.dateTime = dateTime;
 	}
 
 
+    /**
+     * Returns type of exposure ( Direct or Indirect)
+     *
+     * @return exposureType
+     */
 	public String getExposureType() { //Exposure getter
 		return exposureType;
 		
 	}
 
 
+    /**
+     * @param exposureType Accepts String , it should contain only two values either D or I i.e D Indicates Direct Exposure
+     *                     , I Indicates Indirect Exposure
+     * @throws IllegalArgumentException If Exposure Type is other than D or I
+     */
 	public void setExposureType(String exposureType){ // Exposure setter
 		
 			if(exposureType.equals("D")||exposureType.equals("I")){//D - direct exposure 
 				this.exposureType = exposureType; //I - indirect exposure
 			}
 			else{
-				throw new IllegalArgumentException();
+				throw  new IllegalArgumentException("Exposure type should be direct or indirect exposure ('D' or 'I')");
 			}
 		}
-	
+
+	 /**
+     * @return hash value which is unique to compare Objects here uniqueness is on patientId, dateTime
+     */
 	@Override
-	public boolean equals(Object obj) { /**
-	checks whether the two objects are equal or not*/
+	public int hashCode() {
+		return Objects.hash(dateTime, patientId);
+	}
+
+	 /**
+     * @param obj It accepts Object as a parameter
+     * @return boolean true only when both references that are pointing to same object
+     *         Multiple invocations of it, should return same result, unless any of the object properties is modified that is being used in the equals() method implementation.
+     */
+	@Override
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -62,41 +107,21 @@ public class Exposure {
 		if (getClass() != obj.getClass())
 			return false;
 		Exposure other = (Exposure) obj;
-		if (dateTime == null) {
-			if (other.dateTime != null)
-				return false;
-		} else if (!dateTime.equals(other.dateTime))
-			return false;
-		if (patientid == null) {
-			if (other.patientid != null)
-				return false;
-		} else if (!patientid.equals(other.patientid))
-			return false;
-		return true;
-	}
-	
-	/* (non-JavaDoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {/**
-	If the objects are equal the hashCode will be same for the two objects else the hashCode will be different*/
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dateTime == null) ? 0 : dateTime.hashCode());
-		result = prime * result + ((patientid == null) ? 0 : patientid.hashCode());
-		return result;
+		return Objects.equals(dateTime, other.dateTime) && Objects.equals(patientId, other.patientId);
 	}
 
+	 /**
+     * Print the Exposure Class Object values like PatientId,dateTime,exposureType
+     * @return string with all values declared at class level
+     */
+    @Override
+    public String toString() {
+        return "Exposure{" +
+                "patientId=" + patientId +
+                ", dateTime=" + dateTime +
+                ", exposureType='" + exposureType +
+                "}";
+    }
 
-	/* (non-JavaDoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	
-	@Override
-	public String toString() { /**
-	To print the objects*/
-		return patientid+"\t\t"+dateTime+"\t"+exposureType;
-	}
 
 }
